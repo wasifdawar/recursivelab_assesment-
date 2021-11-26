9bdbb1bb8a03e7fadf15f95c4e250bc0f40b863d
wasifdawar <m.wasifdawar@gmail.com>
2021-11-27 03:02:08 +0500

    Adding cards component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,2 +1,22 @@
-lds;
-lsd;
+import React, { props } from "react";
+import { CardList } from "../App";
+import { IncomeExpenses } from "./IncomeExpenses";
+
+const Card = ({ card }) => {
+  return (
+    <div className="card">
+      <div>
+        <h2>{card.heading}</h2>
+      </div>
+      <div>
+        <p>Current APY: {card.current}</p>
+        <p>Amount Deposited: {card.deposit}</p>
+        <p>Accured Interest:{card.interest}</p>
+      </div>
+      <IncomeExpenses />
+      <div>Amount: {card.amount}</div>
+    </div>
+  );
+};
+
+export default Card;
